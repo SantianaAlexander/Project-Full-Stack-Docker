d526edecfe0caa947dafba3ec91e0109480f944b
Alexander Santiana <santiana.a@iisgalvanimi.edu.it>
2025-05-25 17:46:03 +0000

    non va cors e non va mongodb

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'client';
+  messaggio : any;
+  constructor(private http: HttpClient)
+  {
+    this.http.get<any>("https://5000-santianaale-projectfull-xlz68r234nj.ws-eu119.gitpod.io/simple_json")
+    .subscribe(
+      (data) => this.messaggio = data
+    )
+  }
 }
